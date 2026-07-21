0da2863c1d002688b0a0ba2b4ef299f157c1a2c8
pyldmkdj-star <pyldmkdj@gmail.com>
2026-07-21 14:08:14 +0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'hadshot-roshonot-v1';
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  // Pass through fetch requests
+});
